b03381a27b3940056a9696489d2d7cd42dfe83b3
unknown <arghochakma@gmail.com>
2020-11-28 11:14:02 -0800

    logic

---
+++ script.js
@@ -0,0 +1,43 @@
+class{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear(){
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(Number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+}
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operations]');
+const equalsButtons = document.querySelectorAll('[data-equals]');
+const deleteButtons = document.querySelectorAll('[data-delete]');
+const allClearButtons = document.querySelectorAll('[data-all-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
